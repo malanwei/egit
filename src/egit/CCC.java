4ea5a56687723ea4e84a3887bc28704a7318846e
malanwei <1061764954@qq.com>
2018-05-16 19:46:20 +0800

    cchongtu

--- src/egit/CCC.java
+++ src/egit/CCC.java
@@ -2,7 +2,7 @@ package egit;
 
 public class CCC {
 	public static void main(String[] args) {
-		System.out.println("666666+3333333333333333333");
+		System.out.println("666666+3333333333333333333+chongtu");
 		
 	}
 }
